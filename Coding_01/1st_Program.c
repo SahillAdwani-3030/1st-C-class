f17b986f5bd41b5ab65ca7fd601400635351db90
Sahill Adwani <sadwani3030@floridapoly.edu>
2025-08-22 14:37:20 +0000

    Created 1st Programme

---
+++ Coding_01/1st_Program.c
@@ -0,0 +1,10 @@
+/*
+Sahill Adwani
+First Program
+*/
+
+#include <stdio.h>    //stdio.h - Standard input output
+                            //.h - Header File
+int main(void){
+    printf("Hello World\n");
+}  
